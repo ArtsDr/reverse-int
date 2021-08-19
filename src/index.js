66a1d1ae40fdb92dcc382e733010835b15667ef3
Artem Drozd <artsdrd@gmail.com>
2021-08-19 16:30:46 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    let i = n
+    let result = 0
+
+    while (i != 0) {
+        result = (10 * result) + (i % 10)
+        i = Math.trunc(i / 10)
+    }
+    if (n < 0) {
+        return result * -1
+    }
+    return result
 }
